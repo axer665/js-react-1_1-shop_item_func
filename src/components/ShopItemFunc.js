4aee89791d80c89d0df947c4e08bc2f17354d71a
Misha <vellsevul@mail.ru>
2024-01-25 09:33:52 +0300

    Initial commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+function ShopItemFunc(props) {
+    const { item } = props;
+    const price = typeof item.price === "number" ? item.price.toFixed(2) : item.price; 
+    
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">{item.descriptionFull}</div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">
+                    {item.currency}
+                    {price}
+                </div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.object.isRequired,
+};
+
+export default ShopItemFunc;
